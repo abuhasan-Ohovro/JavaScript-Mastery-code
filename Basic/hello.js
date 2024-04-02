a39419a311fc2e8d19896c22cd2f158aa40c6743
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-04-02 09:56:58 +0600

    first code

--- Basic/hello.js
+++ Basic/hello.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const user = {
+    username : "hasan",
+    price : 199,
+
+    welcomeMassage : function(){
+        console.log(`${this.username}, Welcome to our website`);
+    }
+}
+user.welcomeMassage()
+user.username = "Shainndha"
+user.welcomeMassage()
